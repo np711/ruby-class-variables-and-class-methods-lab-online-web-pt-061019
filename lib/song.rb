83fdfcda661d847019e729998125849e0292385b
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-28 15:52:21 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song 
+  
+end 
+  
+  def Song.count 
+    @@count = 0 
+    
+  end 
+  
+    def Song.artists
+    end 
+    
+    def Song.genres 
+      
+    end 
+    
+    def Song. genre_count 
+      {"rap" => 5, "rock" => 1, 
+      "country" => 3}
+      
+  
+      
+      
