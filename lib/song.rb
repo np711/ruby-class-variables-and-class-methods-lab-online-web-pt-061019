dc1002a9c19766a8015bf2bffc9c8b49f4dbb5b2
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-28 15:58:14 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song 
+  
+  def Song.count 
+    @@count = 0 
+    
+  end 
+  
+    def Song.artists
+    end 
+    
+    def Song.genres 
+      
+    end 
+    
+    def Song. genre_count 
+      {"rap" => 5, "rock" => 1, 
+      "country" => 3}
+      
+      def Song.artists_count
+        
+      end 
+      
+  
+      
+      
